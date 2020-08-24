d752c03ae3aa9f2247fe6b3fcdabc1cef839f4b5
kambar98 <kamilbarul123@gmail.com>
2020-08-24 18:00:29 +0200

    routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from "@angular/router";
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -33,8 +33,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
