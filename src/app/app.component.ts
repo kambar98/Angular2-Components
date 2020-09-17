a64522eb600e4ef7961f6e860e7ba05664c70767
kambar98 <kamilbarul123@gmail.com>
2020-09-17 18:13:52 +0200

    Basic forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 
-  ngOnInit() {}
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+  }
+
 }
