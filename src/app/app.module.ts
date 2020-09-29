8b09ebb14b2a3e7882c3198486b0a91009ff21d1
kambar98 <kamilbarul123@gmail.com>
2020-09-29 17:20:33 +0200

    Http and backend interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ShortenPipe } from './short.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShortenPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
