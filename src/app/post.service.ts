b48cb619716569822bcf7398c1ec28e625ed0ba9
kambar98 <kamilbarul123@gmail.com>
2020-09-29 21:43:26 +0200

    Adding query params

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -21,9 +21,14 @@ export class PostService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http.get<{ [key: string]: Post }>
       ('https://nauka-angular-3d163.firebaseio.com/posts.json', {
-        headers: new HttpHeaders({"custom-header":hello})
+        headers: new HttpHeaders({ "custom-header": 'hello' }),
+        params: searchParams
+        
       }).
       pipe(map(responseData => {
         const postsArray: Post[] = [];
