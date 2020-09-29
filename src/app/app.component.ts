88cf6969591c969cbd7c438950d3009c90254e88
kambar98 <kamilbarul123@gmail.com>
2020-09-29 18:01:08 +0200

    sending post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,9 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(
+      'https://nauka-angular-3d163.firebaseio.com/posts.json',
+      postData).subscribe(responseData => { console.log(responseData) });
   }
 
   onFetchPosts() {
