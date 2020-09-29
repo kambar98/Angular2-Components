7e4e40ac2810060c9d8884d5ce22431b75744ba9
kambar98 <kamilbarul123@gmail.com>
2020-09-29 21:11:07 +0200

    clearing post

---
+++ src/app/post.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+
+  constructor(private http: HttpClient) { }
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http.post<{ name: string }>(
+      'https://nauka-angular-3d163.firebaseio.com/posts.json',
+      postData).subscribe(responseData => { console.log(responseData) });
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post }>
+      ('https://nauka-angular-3d163.firebaseio.com/posts.json').
+      pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key })
+          }
+        }
+        return postsArray;
+      }))
+     
+  }
+
+  deletePosts() {
+    return this.http.delete('https://nauka-angular-3d163.firebaseio.com/posts.json');
+  }
+}
