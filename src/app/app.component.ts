050b4d4ab8a2e78b33741946ef6ae3b287d5c219
kambar98 <kamilbarul123@gmail.com>
2020-09-29 18:06:01 +0200

    getting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -21,10 +23,15 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://nauka-angular-3d163.firebaseio.com/posts.json').
+      subscribe(posts => { console.log(posts); });
   }
 }
